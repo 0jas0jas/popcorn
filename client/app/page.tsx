419ec17d2cc98b87c349b4e3bc090f46c930d787
adityash23 <aditya3242@gmail.com>
2024-04-01 14:21:17 -0400

    minor change to site display

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <div className="col-span-2 flex items-center justify-center">
           <div className="text-center p-20 text-8xl"><span className="text-blue-400">Pop</span>corn</div>
         </div>
-        <div className="col-span-2 flex items-center justify-center relative">
+        <div className="col-span-2 flex items-center justify-center">
           
           <Input className="text-center h-12 w-full p-2 rounded-lg justify-between bg-white text-black md:w-3/4 lg:w-1/3 pl-12" placeholder="Enter Stuff here"/>
         </div>
