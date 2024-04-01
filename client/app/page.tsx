6a790937ea680a8e6bc2774449dec097f9cdf811
Ojas <ojas1807@gmail.com>
2024-04-01 18:33:27 -0400

    FINALLY CONNECTED YAYYYY

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,42 @@
-import React from "react";
+"use client"; // This is a client component 👈🏽
+
 import { Input } from "@nextui-org/react";
+import React, { useEffect, useState } from "react";
+
+
+function returnMovies(playlistUrl: string){
+
+  const playlistID = (playlistUrl.slice(34, 56));
+  const serverURL = "http://127.0.0.1:8080/api/fetchMovies";
+  const temp = serverURL.concat("/");
+  const finalURL = temp.concat(playlistID);
+  console.log(finalURL);
+
+  const [message, setMessage] = useState(null);
+  const [data, setData] = useState(null);
+  const [response, setResponse] = useState(null);
+
+  useEffect(() => {
+    const go = async () => {
+      const response = await fetch(finalURL, {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        }, });
+      const data = await response.json();
+      setMessage(data.message);
+    };
+    go();
+  },[])
+
+  console.log(message);
+  return <div>{message}</div>
+}
 
 export default function Home() {
   return (
     <main>
+      <div> {returnMovies("https://open.spotify.com/playlist/33i9dQZF1EQpj7X7UK8OOF?si=adb5830c67094b2f")} </div>
       <div className="grid grid-cols-2 p-12 gap-3 place-content-center">
         <div className="col-span-1 h-24">Logo</div>
         <div className="col-span-1 h-24 text-end">Aditya <br></br> Ojas</div>
--- server/server.py
+++ server/server.py
@@ -1,3 +1,23 @@
-from flask import flask, jsonify
+from flask import Flask, jsonify
+from flask_cors import CORS
 
+#app instance
 
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/api/home", methods = ['GET'])
+def returnhome():
+    print("USED HOME")
+    return jsonify({
+        'message': "Hello World!"
+    })
+
+@app.route("/api/fetchMovies/<id>", methods = ['POST'])
+def returnMovies(id):
+    print(id)
+    if(id == "37i9dQZF1EQpj7X7UK8OOF"): return jsonify({ 'message': "GOT IT!"})
+    return jsonify({'message': "NOPE!"})
+
+if __name__ == "__main__":
+    app.run(debug = True, port = 8080)
